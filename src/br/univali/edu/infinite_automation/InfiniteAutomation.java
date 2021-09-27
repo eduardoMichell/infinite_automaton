21ebb0edad4b606705a7b255fe8b02a384fcb6b5
Eduardo Michel <eduardo@logikee.com.br>
2021-09-27 20:48:05 -0300

    first commit

---
+++ src/br/univali/edu/infinite_automation/InfiniteAutomation.java
@@ -0,0 +1,17 @@
+package br.univali.edu.infinite_automation;
+
+public class InfiniteAutomation {
+    private String[][] automation;
+
+    public InfiniteAutomation(String[][] automation) {
+        this.automation = automation;
+    }
+
+    public String[][] getAutomation() {
+        return automation;
+    }
+
+    public void setAutomation(String[][] automation) {
+        this.automation = automation;
+    }
+}
